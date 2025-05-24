6733368a69419e3a902010b9a591a4befdd8c782
haman-1608 <manhmk3944@ut.edu.vn>
2025-05-25 00:49:52 +0700

    header1

---
+++ components/header.php
@@ -0,0 +1,23 @@
+<header>
+            <div class="container">
+                <div class="logo">
+                    <a href="index.php"><img src="imgs/Logo2.png" alt="Good Optic"></a>
+                </div>
+                <div class="icon-search">
+                    <a href="#"><img src="imgs/icon-header/cil--search.svg" alt="Tìm kiếm"></a>
+                </div>
+                <div class="icon-cart">
+                    <a href="#"><img src="imgs/icon-header/solar--cart-4-bold.svg" alt="Giỏ hàng"></a>
+                </div>
+            <nav>
+                <ul class="menu">
+                    <li><a href="index.php">Trang Chủ</a></li>
+                    <li><a href="#">Gọng Kính</a></li>
+                    <li><a href="#">Kính Râm</a></li>
+                    <li><a href="#">Tròng Kính</a></li>
+                    <li><a href="#">Giới Thiệu</a></li>
+                    <li><a href="#">Liên Hệ</a></li>
+                </ul>
+            </nav>
+            </div>
+        </header>
