259899c8f4b697f11f45afcb4bb95406fc0cb8c3
Le-Hung-2614 <BlueBim123@gmail.com>
2025-05-27 19:46:14 +0700

    Update database

--- database/GoodOptic_db.sql
+++ database/GoodOptic_db.sql
@@ -42,3 +42,203 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `admins`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+-- --------------------------------------------------------------
+--
+-- Table structure for table `categories`
+--
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Dumping data for table `categories`
+--
+INSERT INTO `categories` (`id`, `name`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Kinh Ram', 'kinh-ram', 'Active', NULL, NULL),
+(2, 'Trong Kinh', 'trong-kinh', 'Active', NULL, NULL),
+(3, 'Kinh Can', 'kinh-can', 'Active', NULL, NULL),
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_slug_unique` (`slug`);
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+-- --------------------------------------------------------------
+--
+-- Table structure for table `target`
+--
+CREATE TABLE `target` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `target`
+--
+
+INSERT INTO `target` (`id`, `name`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Trẻ em', 'tre-em', 'Active', NULL, NULL),
+(3, 'Người lớn', 'nguoi-lon', 'Active', NULL, NULL),
+
+--
+-- Indexes for table `target`
+--
+ALTER TABLE `target`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `target_slug_unique` (`slug`);
+
+--
+-- AUTO_INCREMENT for table `target`
+--
+ALTER TABLE `target`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `UV`
+--
+CREATE TABLE `UV` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `UV`
+--
+
+INSERT INTO `UV` (`id`, `name`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'UV 400', 'UV-400', 'Active', NULL, NULL),
+(2, 'UV 380', 'UV-380', 'Active', NULL, NULL),
+(3, 'UV 100%', 'UV-100%', 'Active', NULL, NULL);
+(4, 'UV A/B', 'UV-A/B', 'Active', NULL, NULL);
+
+--
+-- Indexes for table `UV`
+--
+ALTER TABLE `UV`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UV_slug_unique` (`slug`);
+
+--
+-- AUTO_INCREMENT for table `UV`
+--
+ALTER TABLE `UV`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+-- --------------------------------------------------------------
+
+--
+-- Table structure for table `Material`
+--
+CREATE TABLE `Material` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `Material`
+--
+
+INSERT INTO `Material` (`id`, `name`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Nhựa cứng', 'Nhua-cung', 'Active', NULL, NULL),
+(2, 'Nhựa dẻo', 'Nhua-deo', 'Active', NULL, NULL),
+
+--
+-- Indexes for table `Material`
+--
+ALTER TABLE `Material`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Material_slug_unique` (`slug`);
+
+--
+-- AUTO_INCREMENT for table `Material`
+--
+ALTER TABLE `Material`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+-- --------------------------------------------------------------
+
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(1,)
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `brands_slug_unique` (`slug`);
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+-- --------------------------------------------------------
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unit` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` double NOT NULL,
+  `discount` double NOT NULL DEFAULT 0,
+  `disscounted_price` double NOT NULL = price*(1 - discount / 100),
+  `images` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `brand_id` bigint(20) UNSIGNED NOT NULL,
+  `target_id` bigint(20) UNSIGNED NOT NULL,
+  `UV_id` bigint(20) UNSIGNED NOT NULL,
+  `Material_id` bigint(20) UNSIGNED NOT NULL,
+  `status` enum('Active','Inactive') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
